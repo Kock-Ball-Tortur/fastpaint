b3a006cb2ed9d20800a33fe844f95609ce1beacc
Snoilt <paul@oellers.net>
2022-11-17 13:40:08 +0100

    test: Clipboard testytest
    Page automatically gets text from clipboard and pastes it into website
    element

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-
+let getTextTest: string
+  navigator.clipboard
+    .readText()
+    .then(clipText => getTextTest = clipText)
 </script>
 
 <main>
-
+  <div id="imgContainer">
+    <p>{getTextTest}</p>
+  </div>
 </main>
 
 <style>
-
 </style>
